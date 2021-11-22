dbcb71c4730534f802ac0b82f1dfcd6e346286a8
Matthew <phillips.matthew829@gmail.com>
2021-11-22 12:17:56 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -16,9 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let toyUrl = "http://localhost:3000/toys"; //toyUrl is the URL that all of the toy info is stored at.
 
+//Create a toy Object, where for each key, the value is a reference to where to find it
 /*
-let toy = {
-
+const toy = {
+  Name //String
+  Image //URL
+  Likes
+  ID 
 }
 */
 
@@ -33,21 +37,29 @@ function loadToys(){ //loadToys is the function for fetching all of the toy Obje
   //GET
 }
 
-function submitToy(){
-  fetch(toyUrl, {
-    method: "POST", 
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    }
+document.getElementsByClassName("submit").addEventListener("click", (event) => {
+  event.preventDefault(); //Prevent reloading the page after clicking the button.
+  function addToy(){ //addToy is the function for submitting a new toy to the list.
+    fetch(toyUrl, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+      body: JSON.stringify({
+        //Toy Object:
+        //"Name"
+        //"Image"
+        //"Likes": 0
+      })
+  
+    })
+  }
+});
 
-    //Body
-
-  })
-}
-
-document.addEventListener("click", likeToy);
+//for(const in ){//Use a For In Loop so that, for each toy...}
+document.addEventListener("click", likeToy); //Place Event Listener on Like button of each toy.
 //PATCH
-function likeToy(){
+function likeToy(){ //likeToy is the function for updating the number of likes on a toy
 
 }
