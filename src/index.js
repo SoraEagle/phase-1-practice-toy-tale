65e84c42dc32f0eb7bcf6e17206a957a7efff738
Matthew <phillips.matthew829@gmail.com>
2021-11-22 11:39:52 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,41 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let toyUrl = "http://localhost:3000/toys"; //toyUrl is the URL that all of the toy info is stored at.
+
+/*
+let toy = {
+
+}
+*/
+
+//likeToy is a function for updating the number of likes 
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM has loaded");
+
+});
+
+function loadToys(){ //loadToys is the function for fetching all of the toy Objects.
+  //GET
+}
+
+function submitToy(){
+  fetch(toyUrl, {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+
+    //Body
+
+  })
+}
+
+document.addEventListener("click", likeToy);
+//PATCH
+function likeToy(){
+
+}
