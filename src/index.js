f3cf2aed9eb29b9c85ae0ebb431cdf7304b95f65
Matthew <phillips.matthew829@gmail.com>
2021-11-24 12:17:58 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -15,38 +15,51 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let toyUrl = "http://localhost:3000/toys"; //toyUrl is URL where info of all toys is stored at.
-/* const toy = { Create toy Object; For each key, the value is a reference to where to find it
-  Name:  document.getElementById(""); //Get from nameInput.value
-  Image:  document.getElementById(""); //Get from imgInput.value
-  Likes: getElementById("");}*/
-let nameInput = document.getElementsByTagName("input")[0]; //set new toy's name to innerText.
-nameInput.id = "name-input";
-nameInput.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(event.target.value);
-  toyData.toyName = nameInput.value;
-  console.log(toyData.toyName);
-})
 
-let imgInput = document.getElementsByTagName("input")[1]; //Set new toy's img to innerText.
-imgInput.id = "image-input";
-imgInput.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(event.target.value);
-  toyData.toyImage = imgInput.value;
-  console.log(toyData.toyImage);
+function formSubmit(event){ //For pushing toy info to toys
+    event.preventDefault();
+    // debugger;
+  let nameInput = document.getElementsByTagName("input")[0]; //set new toy's name to .value.
+  nameInput.id = "name-input";
+  toyName = nameInput.value;
+
+  let imgInput = document.getElementsByTagName("input")[1]; //set new toy's image to .value.
+  imgInput.id = "image-input";
+  toyImage = imgInput.value;
+
+  let toy = { //Create a toy Object
+    "name": toyName,
+    "image": toyImage,
+    "likes": 0
+  }
+
+// let imgInput = document.getElementsByTagName("input")[1]; //Set new toy's img to innerText.
+// imgInput.id = "image-input";
+// toy.image = imgInput.value;
+
+fetch(toyUrl, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  body: JSON.stringify(toy) 
 })
+.catch(function (error){
+  alert("Ragnarők!  Unable to create the toy!"); //Alert when error occurs with creating a toy
+  document.body.innerHTML = error.message; //replace page contents with this error message.
+});
+
+alert("New toy has been added!  Press \"OK\" to relaod the page.");
+location.reload();
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM has loaded");
   loadToys();
+  let toyForm = document.getElementsByTagName("form")[0];
+  toyForm.addEventListener("submit", formSubmit);
 });
-
-let toyData = { //Object used to create a new toy.
-  "toyName": "",
-  "toyImage": "",
-  "toyLikes": 0
-}
 
 function loadToys(){ //loadToys is the function for fetching all of the toy Objects.
   fetch(toyUrl, {
@@ -89,27 +102,9 @@ function loadToys(){ //loadToys is the function for fetching all of the toy Obje
   });
 }
 
-let createToy = document.getElementsByTagName("input")[2]; //The Submit button.
-createToy.addEventListener("click", (event) => {
-  event.preventDefault(); //Prevent reloading the page after clicking the button.
-  function addToy(){ //addToy is the function for submitting a new toy to the list.
-    fetch(toyUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify(toyData) 
-    })
-    .catch(function (error){
-      alert("Ragnarők!  Unable to create the toy!"); //Alert when error occurs with creating a toy
-      document.body.innerHTML = error.message; //replace page contents with this error message.
-  });
-  }
-});
+// let createToy = document.getElementsByTagName("input")[2]; //The Submit button.
 
 function updateLikes(event){ //Function to add a like
-      event.preventDefault(); //Prevent refreshing of the page.
       event.target.value;
       let toyID = `${toyUrl}/${event.target.id}`; //http://localhost:3000/(toy's ID #).
       fetch(toyID, {
